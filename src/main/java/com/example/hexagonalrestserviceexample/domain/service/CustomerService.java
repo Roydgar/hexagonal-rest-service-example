94b0f9dc5e6a1bb536a73fcf0ff30f7bc97285c2
Viktor_Gritsaenko <viktor_gritsaenko@epam.com>
2023-09-11 15:19:41 +0300

    Refactor

--- src/main/java/com/example/hexagonalrestserviceexample/domain/service/CustomerService.java
+++ src/main/java/com/example/hexagonalrestserviceexample/domain/service/CustomerService.java
@@ -3,5 +3,5 @@ package com.example.hexagonalrestserviceexample.domain.service;
 import com.example.hexagonalrestserviceexample.domain.model.CustomerDetails;
 
 public interface CustomerService {
-    public CustomerDetails getCustomerDetails(String customerId);
+    CustomerDetails getCustomerDetails(String customerId);
 }
